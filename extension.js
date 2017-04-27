9ff59a260c93c3c41a9378a3eb66639405597217
刘国涛 <liuguotao@zuozh.com>
2017-04-27 14:16:57 +0800

    remove vsix

--- extension.js
+++ extension.js
@@ -3,47 +3,63 @@ const vscode = require('vscode')
 var LINE_SEPERATOR = /\n|\r\n/
 var JSON_SPACE = 4
 
+/**
+ * 
+ * @param {TextEditor} editor 
+ */
+function getRaw(editor) {
+    let raw, range
+    if (!editor.selection.isEmpty) {
+        range = editor.selection
+        raw = editor.document.getText(range)
+    } else {
+        raw = editor.document.getText()
+        let start = new vscode.Position(0, 0)
+        let lines = raw.split(LINE_SEPERATOR)
+        let end = new vscode.Position(lines.length, lines[lines.length - 1].length)
+        range = new vscode.Range(start, end)
+    }
+    return [raw, range]
+}
+
+/**
+ * 
+ * @param {String} raw 
+ */
+function parseJson(raw) {
+    try {
+        return JSON.parse(raw)
+    } catch (jsonParseError) {
+        return undefined
+    }
+}
+
 function activate(context) {
     console.log('激活扩展 <tools>');
-
     let disposable = vscode.commands.registerTextEditorCommand('json.format', function (editor) {
-        let raw = editor.document.getText()
-        let json
-        try {
-            json = JSON.parse(raw)
-        } catch (jsonParseError) {
+        let [raw, range] = getRaw(editor)
+        let json = parseJson(raw)
+        if (!json) {
             vscode.window.showErrorMessage('不合法的JSON字符串')
             return
         }
-        let pretty = JSON.stringify(json, null, JSON_SPACE)
         editor.edit(function (builder) {
-            let start = new vscode.Position(0, 0)
-            let lines = raw.split(LINE_SEPERATOR)
-            let end = new vscode.Position(lines.length, lines[lines.length - 1].length)
-            let allRange = new vscode.Range(start, end)
-            builder.replace(allRange, pretty)
+            builder.replace(range, JSON.stringify(json, null, JSON_SPACE))
         })
     })
     context.subscriptions.push(disposable)
 
     disposable = vscode.commands.registerTextEditorCommand('json.minify', function (editor) {
-        let raw = editor.document.getText()
-        let json
-        try {
-            json = JSON.parse(raw)
-        } catch (jsonParseError) {
+        let [raw, range] = getRaw(editor)
+        let json = parseJson(raw)
+        if (!json) {
             vscode.window.showErrorMessage('不合法的JSON字符串')
             return
         }
-        let pretty = JSON.stringify(json)
         editor.edit(function (builder) {
-            let start = new vscode.Position(0, 0)
-            let lines = raw.split(LINE_SEPERATOR)
-            let end = new vscode.Position(lines.length, lines[lines.length - 1].length)
-            let allRange = new vscode.Range(start, end)
-            builder.replace(allRange, pretty)
+            builder.replace(range, JSON.stringify(json))
         })
-    });
+    })
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
