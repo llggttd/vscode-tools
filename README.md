eee1da77f2fb089763806594ebcf92d4d73c1404
huangyou <huangyou@2dfire.com>
2019-01-22 11:10:47 +0800

    add url encode

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 ## Features
 
+支持指定选区，在不指定选区时，默认使用当前编辑器中的所有文本。
+
 - json.format 格式化JSON字符串
 
 - json.minify 压缩JSON字符串
@@ -12,7 +14,14 @@
 
 - date.currentTime 在光标处插入当前时间，如：14:24:17
 
+- url.encodeURI URL编码
 
+- url.decodeURI URL解码
+
+- url.encodeURIComponents URL组件编码
+
+- url.decodeURIComponents URL组件解码
+      
 ## Requirements
 
 - moment
--- extension.js
+++ extension.js
@@ -35,47 +35,105 @@ function parseJson(raw) {
     }
 }
 
+/**
+ * 格式化JSON字符串
+ */
+const formatJson = vscode.commands.registerTextEditorCommand('json.format', function (editor) {
+    let [raw, range] = getCurrentSelection(editor)
+    let json = parseJson(raw)
+    if (!json) {
+        vscode.window.showErrorMessage('不合法的JSON字符串')
+        return
+    }
+    editor.edit(function (builder) {
+        builder.replace(range, JSON.stringify(json, null, JSON_SPACE))
+    })
+})
+
+/**
+ * 压缩JSON字符串
+ */
+const minifyJson = vscode.commands.registerTextEditorCommand('json.minify', function (editor) {
+    let [raw, range] = getCurrentSelection(editor)
+    let json = parseJson(raw)
+    if (!json) {
+        vscode.window.showErrorMessage('不合法的JSON字符串')
+        return
+    }
+    editor.edit(function (builder) {
+        builder.replace(range, JSON.stringify(json))
+    })
+})
+
+/**
+ * 插入当前时间
+ */
+const currentTime = vscode.commands.registerTextEditorCommand('date.currentTime', function (editor) {
+    editor.edit(function (builder) {
+        builder.insert(editor.selection.start, moment().format('HH:mm:ss'))
+    })
+})
+
+/**
+ * 插入当前日期
+ */
+const currentDate = vscode.commands.registerTextEditorCommand('date.currentDate', function (editor) {
+    editor.edit(function (builder) {
+        builder.insert(editor.selection.start, moment().format('YYYY-MM-DD'))
+    })
+})
+
+/**
+ * encodeURI
+ */
+const urlEncode = vscode.commands.registerTextEditorCommand('url.encodeURI', function (editor) {
+    let [raw, range] = getCurrentSelection(editor)
+    editor.edit(function (builder) {
+        builder.replace(range, encodeURI(raw))
+    })
+})
+
+/**
+ * decodeURI
+ */
+const urlDecode = vscode.commands.registerTextEditorCommand('url.decodeURI', function (editor) {
+    let [raw, range] = getCurrentSelection(editor)
+    editor.edit(function (builder) {
+        builder.replace(range, decodeURI(raw))
+    })
+})
+
+/**
+ * encodeURIComponents
+ */
+const urlComponentsEncode = vscode.commands.registerTextEditorCommand('url.encodeURIComponents', function (editor) {
+    let [raw, range] = getCurrentSelection(editor)
+    editor.edit(function (builder) {
+        builder.replace(range, encodeURIComponent(raw))
+    })
+})
+
+/**
+ * decodeURIComponent
+ */
+const urlComponentsDecode = vscode.commands.registerTextEditorCommand('url.decodeURIComponents', function (editor) {
+    let [raw, range] = getCurrentSelection(editor)
+    editor.edit(function (builder) {
+        builder.replace(range, decodeURIComponent(raw))
+    })
+})
+
+
 function activate(context) {
     console.log('激活扩展 <tools>');
-    let formatJson = vscode.commands.registerTextEditorCommand('json.format', function (editor) {
-        let [raw, range] = getCurrentSelection(editor)
-        let json = parseJson(raw)
-        if (!json) {
-            vscode.window.showErrorMessage('不合法的JSON字符串')
-            return
-        }
-        editor.edit(function (builder) {
-            builder.replace(range, JSON.stringify(json, null, JSON_SPACE))
-        })
-    })
     context.subscriptions.push(formatJson)
-
-    let minifyJson = vscode.commands.registerTextEditorCommand('json.minify', function (editor) {
-        let [raw, range] = getCurrentSelection(editor)
-        let json = parseJson(raw)
-        if (!json) {
-            vscode.window.showErrorMessage('不合法的JSON字符串')
-            return
-        }
-        editor.edit(function (builder) {
-            builder.replace(range, JSON.stringify(json))
-        })
-    })
-    context.subscriptions.push(minifyJson);
-
-    let currentTime = vscode.commands.registerTextEditorCommand('date.currentTime', function (editor) {
-        editor.edit(function (builder) {
-            builder.insert(editor.selection.start, moment().format('HH:mm:ss'))
-        })
-    })
-    context.subscriptions.push(currentTime);
-
-    let currentDate = vscode.commands.registerTextEditorCommand('date.currentDate', function (editor) {
-        editor.edit(function (builder) {
-            builder.insert(editor.selection.start, moment().format('YYYY-MM-DD'))
-        })
-    })
-    context.subscriptions.push(currentDate);
+    context.subscriptions.push(minifyJson)
+    context.subscriptions.push(currentTime)
+    context.subscriptions.push(currentDate)
+    context.subscriptions.push(urlEncode)
+    context.subscriptions.push(urlDecode)
+    context.subscriptions.push(urlComponentsEncode)
+    context.subscriptions.push(urlComponentsDecode)
 }
 exports.activate = activate;
 
